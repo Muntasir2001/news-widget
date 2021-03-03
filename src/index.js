5a60a29c2decf3a97b50cd46ca7f207551a1f614
Muntasir <muntasirenamalhuda@gmail.com>
2021-03-03 17:23:56 +0000

    added FallbackComponent

--- src/index.js
+++ src/index.js
@@ -4,8 +4,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+function ErrorFallback({error, resetErrorBoundary}) {
+  return (
+    <div role="alert">
+      <p>Something went wrong:</p>
+      <pre>{error.message}</pre>
+      <button onClick={resetErrorBoundary}>Try again</button>
+    </div>
+  )
+}
+
 ReactDOM.render(
-  <ErrorBoundary>
+  <ErrorBoundary
+    FallbackComponent={ErrorFallback}
+  >
     <React.StrictMode>
       <App />
     </React.StrictMode>
